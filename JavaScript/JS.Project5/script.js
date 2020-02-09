da1dbc7d9cfa94c641cc52650af3d351027a0d7f
jaime velazquez <jaimevelazquez@Admins-MBP.attlocal.net>
2020-02-09 11:12:28 -0600

    JS Project5

--- JavaScript/JS.Project5/script.js
+++ JavaScript/JS.Project5/script.js
@@ -1,15 +1,20 @@
 var catArr = ["images/cat1.jpg", "images/cat2.jpg", "images/cat3.jpg"];
 var i = 1;
 
+var img = document.querySelector("img")
+var but = document.getElementById("next")
+var but2 = document.getElementById("previous")
+
+
+
+
 // For Next button
-var but = document.querySelector("button")
-console.log(but);
-
-
 
 but.addEventListener("click", function() {
     document.querySelector("img").setAttribute("src", catArr[i]);
    i ++;
+   console.log(catArr[i]);
+   console.log(i);
   if (i === catArr.length){
       i = 0;
   };
@@ -17,32 +22,17 @@ but.addEventListener("click", function() {
 
 // For Previous button
 
-var but2 = document.querySelector(".pic")
-console.log(but2);
-var img = document.querySelector("img")
-
-but2.addEventListener("click", function () {
-  if  (i < catArr.length && i > 0)  {
-       i = i - 1;
-       img.setAttribute("src", catArr[i]);
-       console.log(i);
-       console.log(catArr[i]);
-   } else {
-       i = 0;
-       img.setAttribute("src", catArr[i]);
-       console.log(i);
-       console.log(catArr[i]);
-   }  
+but2.addEventListener("click", function() {
+    img.setAttribute("src", catArr[i]);
+    i --;
+  if (i == -1){
+      i = catArr.length -1;
+  }
 });
 
 
 
 
-// but2.addEventListener("click", function() {
-//     i --;
-//     document.querySelector("img").setAttribute("src", catArr[i]);
-  
-//   if (i == catArr.length){
-//       i = 0;
-//   }
-// });
+
+
+
